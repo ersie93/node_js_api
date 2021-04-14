2fc171159deffaa0762baf98c5317afdfc21c3ee
ryan erswell <ersie93@googlemail.com>
2021-04-14 17:06:02 +0100

    setup mongodb

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
 
 //import routes
 const postRoutes = require('./routes/post');
@@ -10,5 +15,16 @@ app.use(morgan('dev'));
 
 app.use('/', postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`listening on port: ${port}`));
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,8 @@
 exports.getPosts = (req, res) => {
-	res.send("hello world");
+	res.json({
+		posts: [
+			{ author: "Ryan Erswell"},
+			{ content: "hello it is also just me!"}
+		]
+	});
 };
