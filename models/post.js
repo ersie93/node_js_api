8a37c89e09c84728645a28c62fde76355e0de911
ryan erswell <ersie93@googlemail.com>
2021-04-14 17:28:21 +0100

    added post model

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+	author: {
+		type: String,
+		required: "Please enter the authors name",
+		minlength: 3,
+		maxlength: 50,
+	},
+	title: {
+		type: String,
+		required: "Please enter a title",
+		minlength: 5,
+		maxlength: 100
+	},
+	content: {
+		type: String,
+		required: "Must be content with post",
+		minlength: 100,
+		maxlength: 2000
+	}
+})
+
+module.exports= mongoose.model("Post", postSchema);
