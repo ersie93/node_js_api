c32efcef7fcaebf3286262cc8f368ded58fc8f47
ryan erswell <ersie93@googlemail.com>
2021-04-14 20:27:12 +0100

    returned response for post

--- controllers/post.js
+++ controllers/post.js
@@ -11,5 +11,15 @@ exports.getPosts = (req, res) => {
 
 exports.createPost = (req, res) => {
 	const post = new Post(req.body);
-	console.log("creating post", req.body);
+
+	post.save((err, result) => {
+		if(err){
+			return res.status(400).json({
+				error: err
+			})
+		}
+		res.status(200).json({
+			post: result
+		})
+	})
 };
